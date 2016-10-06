e87f13546590d589066a34b50541d2bdb7551b13
Casper Winther Hansen <cwh@ennova.com>
2016-10-06 15:15:47 +0200

    Test

--- components.js
+++
@@ -1,1 +0,0 @@
-exports.AutoSize = require('./lib/AutoSize').AutoSize;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AutoSize } from "./AutoSize";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [AutoSize],
+    exports: [AutoSize],
+    entryComponents: [AutoSize]
+})
+export class AutoSizeModule {
+}
