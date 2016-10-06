44d1d584b2463097b67a6240027e6a637bc589d7
Casper Winther Hansen <cwh@ennova.com>
2016-10-06 15:49:08 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AutoSize } from "./AutoSize";
 
+export * from "./AutoSize";
+
 @NgModule({
     imports: [CommonModule],
     declarations: [AutoSize],
