84561a905d14f2376aabc8e9af09a3814e38d488
Casper Winther Hansen <cwh@ennova.com>
2016-10-07 08:24:24 +0200

    Indentation

--- src/AutoSize.ts
+++ src/AutoSize.ts
@@ -1,21 +1,23 @@
 import { Directive, ElementRef, Input, Renderer, HostListener } from '@angular/core';
-@Directive({ selector: 'textarea[autosize]' })
+@Directive({
+     selector: "textarea[autosize]"
+})
 export class AutoSize {
-  @HostListener("input", ["$event.target"])
-  onInput(textArea: HTMLTextAreaElement): void {
-      this.adjust();
-  }
+    @HostListener("input", ["$event.target"])
+    onInput(textArea: HTMLTextAreaElement): void {
+        this.adjust();
+    }
 
-  constructor(public element: ElementRef) {
-  }
+    constructor(public element: ElementRef) {
+    }
 
-  ngAfterContentChecked(): void {
-      this.adjust();
-  }
+    ngAfterContentChecked(): void {
+        this.adjust();
+    }
 
-  adjust(): void {
-      this.element.nativeElement.style.overflow = "hidden";
-      this.element.nativeElement.style.height = "auto";
-      this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
-  }
+    adjust(): void {
+        this.element.nativeElement.style.overflow = "hidden";
+        this.element.nativeElement.style.height = "auto";
+        this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
+    }
 }
