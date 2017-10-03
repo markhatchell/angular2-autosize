789f5cfb9493605bf6c0f3b697232e84d2c89202
Casper Winther Hansen <cwh@ennova.com>
2017-10-03 14:26:16 +0200

    Making it work with angular cli

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,10 @@
 export default {
-	entry: 'dist/index.js',
-	dest: 'dist/bundles/autosize.umd.js',
-	sourceMap: false,
-	format: 'umd',
-	moduleName: 'ng.autosize',
-	globals: {
-		'@angular/core': 'ng.core',
-		'@angular/common': 'ng.common',
-		'rxjs/Observable': 'Rx',
-		'rxjs/ReplaySubject': 'Rx',
-		'rxjs/add/operator/map': 'Rx.Observable.prototype',
-		'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-		'rxjs/add/observable/fromEvent': 'Rx.Observable',
-		'rxjs/add/observable/of': 'Rx.Observable'
-	}
+  entry: 'dist/index.js',
+  dest: 'dist/bundles/autosize.umd.js',
+  sourceMap: false,
+  format: 'umd',
+  moduleName: 'ng.autosize',
+  globals: {
+    '@angular/core': 'ng.core'
+  }
 }
